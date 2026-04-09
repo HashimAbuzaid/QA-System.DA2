ef13e739155c63b0820d9309314276b293f5e825
HashimAbuzaid <habuzaid@detroitaxle.com>
2026-04-09 10:10:10 -0400

    Update main.tsx

--- DA-SYSTEM/src/main.tsx
+++ DA-SYSTEM/src/main.tsx
@@ -1,7 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
 import { StrictMode, useEffect, useMemo, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
@@ -67,9 +63,6 @@ function ThemeBoot() {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
     <ThemeBoot />
   </StrictMode>
 );
